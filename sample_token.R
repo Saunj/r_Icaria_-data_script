1ab455290e66c9523aa25771c05ee839ab17960d
Joe-Henry Curtis Sunders <73123679+Saunj@users.noreply.github.com>
2022-11-01 15:23:45 +0000

    Add files via upload

---
+++ sample_token.R
@@ -0,0 +1,39 @@
+# PART1: Define data access parameters
+kRedcapAPIURL <-""
+
+kRedcapTokens <- list(
+  # ICARIA Port Loko - HF01 Lungi Under Fives CHP (script)
+  HF01 = "",
+  # ICARIA Port Loko - HF02 Lunsar CHC
+  HF02 = "",
+  # ICARIA Port Loko - HF03 Mahera CHC (script)
+  HF03 = "",
+  # ICARIA Port Loko - HF04 Masiaka CHC
+  HF04 = "",
+  # ICARIA Port Loko - HF05 Port Loko U5 CHP (script)
+  HF05 = "",
+  # ICARIA Port Loko - HF06 Rogbere -Junction CHC (script)
+  HF06 = "",
+  # ICARIA Tonkolili - HF08 Magburaka Under Fives Clinic (script)
+  HF08 = "",
+  # ICARIA Tonkolili - HF10 Matotoka CHC
+  HF10 = "",
+  # ICARIA Bombali - HF11 Loreto Clinic (script)
+  HF11 = "",
+  # ICARIA Bombali - HF12 Red Cross (Makeni City) CHP (script)
+  HF12 = "",
+  # ICARIA Bombali - HF13 Stocco CHP (script)
+  HF13 = "",
+  # ICARIA Bombali - HF15 Makama CHP (script)
+  HF15 = "",
+  # ICARIA Bombali - HF16 Makeni Government Hospital (script)
+  HF16 = "",
+  # ICARIA Bombali - HF17 Masuba CHC (script)
+  HF17 = ""
+)
+
+cohortToken <- ""
+
+
+
+
